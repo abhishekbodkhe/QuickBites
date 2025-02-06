f4cf13fd54235392f1155305154352f6ac59ac5f
Abhishek <abhikpujari7@gmail.com>
2025-02-06 15:45:44 +0530

    defined entities

--- QuickBites_Backend/src/main/java/com/quickbites/entities/CartItem.java
+++ QuickBites_Backend/src/main/java/com/quickbites/entities/CartItem.java
@@ -1,14 +1,11 @@
 package com.quickbites.entities;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
