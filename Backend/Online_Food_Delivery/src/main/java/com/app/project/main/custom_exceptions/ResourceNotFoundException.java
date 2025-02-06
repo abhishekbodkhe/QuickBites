a04784544a60f050e5f66ff9ef01dbc1e1746ba2
Abhishek <abhikpujari7@gmail.com>
2025-02-06 15:41:54 +0530

    defined entities

--- Backend/Online_Food_Delivery/src/main/java/com/app/project/main/custom_exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.project.main.custom_exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
---
+++ QuickBites_Backend/src/main/java/com/quickbites/enums/USER_ROLE.java
@@ -0,0 +1,7 @@
+package com.quickbites.enums;
+
+public enum USER_ROLE {
+
+	ROLE_CUSTOMER, ROLE_RESTAURANT_OWNER, ROLE_RESTAURANT_MANAGER, ROLE_ADMIN
+
+}
